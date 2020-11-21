814edd864ba52c61d338873f2d59b4755e51c6bb
iojon <>
2020-11-21 17:43:11 -0500

    Disable gamepad for overlay since we don't use it yet

--- crates/injected/build.rs
+++ crates/injected/build.rs
@@ -18,6 +18,10 @@ fn main() {
         .include(env!("CARGO_MANIFEST_DIR").to_owned() + "/imgui")
         .files(&cxx)
         .flag_if_supported("-std=c++17")
+        // ImGui uses XInput1_4.dll by default. These two variables prevents it.
+        // https://github.com/ocornut/imgui/issues/2716
+        .define("IMGUI_IMPL_WIN32_DISABLE_GAMEPAD", "1")
+        .define("IMGUI_IMPL_WIN32_DISABLE_LINKING_XINPUT", "1")
         .compile("cxxbridge");
 
     for bridge in &bridges {
