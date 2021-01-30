c96ef14c28de54903962d6be4e0bd3036f846bde
Hanno <h.baensch.92@gmail.com>
2021-01-30 17:02:53 +0100

    Add cmd line option for starting Spel2 automatically Makes it easier to debug in VS

--- src/injector/main.cpp
+++ src/injector/main.cpp
@@ -2,6 +2,7 @@
 #include <filesystem>
 #include <thread>
 
+#include "cmd_line.h"
 #include "injector.h"
 #include "logger.h"
 
@@ -18,36 +19,74 @@ fs::path get_dll_path() {
     return path.parent_path().concat("\\injected.dll");
 }
 
-int main() {
-    auto path = get_dll_path();
+int main(int argc, char** argv) {
+    CmdLineParser cmd_line_parser(argc, argv);
 
-    if (!fs::exists(path)) {
-        PANIC("DLL not found! %s", path.string().data());
+    auto overlunky_path = get_dll_path();
+
+    if (!fs::exists(overlunky_path)) {
+        PANIC("DLL not found! %s", overlunky_path.string().data());
     }
 
     INFO("Overlunky version: " TOSTRING(GIT_VERSION));
 
-    {
-        INFO("Searching for Spel2.exe process...");
-        bool started = true;
-        Process proc;
-        while (true) {
-            if (auto res = find_process("Spel2.exe")) {
-                proc = res.value();
-                break;
+    Process game_proc = [&cmd_line_parser]() {
+        std::string_view spel2_exe_path = GetCmdLineParam<std::string_view>(
+            cmd_line_parser, "launch_game", "");
+        if (spel2_exe_path.empty() || !fs::exists(spel2_exe_path)) {
+            INFO("Searching for Spel2.exe process...");
+            bool started = true;
+            Process proc;
+            while (true) {
+                if (auto res = find_process("Spel2.exe")) {
+                    proc = res.value();
+                    break;
+                }
+                started = false;
+                std::this_thread::sleep_for(1s);
+            };
+            INFO("Found Spel2.exe PID: %d", proc.info.pid);
+            if (!started) {
+                INFO(
+                    "Game was just started, waiting a few seconds for it to "
+                    "load before injecting...");
+                std::this_thread::sleep_for(1s);
             }
-            started = false;
-            std::this_thread::sleep_for(1s);
-        };
-        INFO("Found Spel2.exe PID: %d", proc.info.pid);
-        if (!started) {
+            return proc;
+        } else {
             INFO(
-                "Game was just started, waiting a few seconds for it to load "
-                "before injecting...");
-            std::this_thread::sleep_for(1s);
+                "Game path was passed on cmd line, launching game directly...");
+
+            char spel2_cmd_line[MAX_PATH];
+            sprintf_s(spel2_cmd_line, "%.*s",
+                      static_cast<int>(spel2_exe_path.size()),
+                      spel2_exe_path.data());
+
+            const std::string spel2_dir =
+                fs::path(spel2_exe_path).parent_path().string();
+
+            STARTUPINFO si{};
+            si.cb = sizeof(si);
+
+            PROCESS_INFORMATION pi{};
+
+            if (CreateProcess(spel2_cmd_line, NULL, NULL, NULL, TRUE, 0, NULL,
+                              spel2_dir.c_str(), &si, &pi)) {
+                WaitForInputIdle(pi.hProcess, 1000);
+
+                INFO("Waiting a few seconds before injecting...");
+                std::this_thread::sleep_for(5s);
+
+                Process proc{pi.hProcess, ProcessInfo{"Spel2", pi.dwProcessId}};
+
+                CloseHandle(pi.hThread);
+
+                return proc;
+            }
         }
-        inject_dll(proc, path.string());
-        call(proc, find_function(proc, path.string(), "run"),
-             (LPVOID)(uint64_t)GetCurrentProcessId());
-    }
+    }();
+
+    inject_dll(game_proc, overlunky_path.string());
+    call(game_proc, find_function(game_proc, overlunky_path.string(), "run"),
+         (LPVOID)(uint64_t)GetCurrentProcessId());
 }
