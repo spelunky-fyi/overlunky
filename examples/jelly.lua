ee1675083ffba215a714fa57179e9292363a80f0
Dregu <dregu@dreg.us>
2021-02-05 08:38:29 +0200

    update simple example

--- examples/jelly.lua
+++ examples/jelly.lua
@@ -2,5 +2,7 @@
 -- spawns a jellyfish above every entrance
 function on_level()
   print("Entered level "..tostring(state.world).."-"..tostring(state.level))
-  spawn_entity(312, 0, 3, false, 0, 0, false)
+  players = get_players()
+  x, y, layer = get_position(players[1].uid)
+  spawn_entity(ENT_TYPE.MONS_MEGAJELLYFISH, x, y+3, layer, 0, 0)
 end
