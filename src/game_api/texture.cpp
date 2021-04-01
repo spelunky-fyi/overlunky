f2b4d9567733079b11462504092ce39ae7a8779d
Hanno <h.baensch.92@gmail.com>
2021-04-01 13:53:35 +0200

    Add texture data access

---
+++ src/game_api/texture.cpp
@@ -0,0 +1,22 @@
+#include "texture.hpp"
+
+#include "memory.hpp"
+
+size_t textures_ptr()
+{
+    static size_t cached_textures_ptr;
+    ONCE(size_t)
+    {
+        auto mem = Memory::get();
+        res = mem.at_exe(
+            decode_pc(mem.exe(),
+                find_inst(mem.exe(), "\x48\x8d"s,
+                    find_inst(mem.exe(), "\x75\x06\x41\x8b\xec\x41\x8b\xf7"s, mem.after_bundle)))) - 8;
+        return res;
+    }
+}
+
+Textures* get_textures()
+{
+    return (Textures*)textures_ptr();
+}
