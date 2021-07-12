00e99595b9fe17534e1e1557e84bd42c77d10c3e
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2021-07-12 18:58:28 +0200

    fix clang formating better

--- src/game_api/entities_monsters.hpp
+++ src/game_api/entities_monsters.hpp
@@ -16,16 +16,16 @@ class RoomOwner : public Monster
 {
   public:
     int32_t room_index;
-    float climb_y_direction;        // when on a rope or ladder
+    float climb_y_direction; // when on a rope or ladder
     uint8_t ai_state;
     uint8_t patrol_timer;
-    uint8_t lose_interest_timer;    // delay in-between attacks
+    uint8_t lose_interest_timer; // delay in-between attacks
     uint8_t unknown_timer1;
-    uint16_t countdown_timer;       // counts down to 0 when a value is forced, can't shot when the timer is running
+    uint16_t countdown_timer; // counts down to 0 when a value is forced, can't shot when the timer is running
     uint8_t unknown1;
-    bool is_patrolling;             // to set off a shopkeeper, combine this with state.shoppie_aggro_levels > 0
-    bool aggro;                     // setting this makes him angry, if it's shopkeeper you get 2 agrro points
-    bool was_hurt;                  // also is set true if you set aggro to true, get's trigger even when whiping
+    bool is_patrolling; // to set off a shopkeeper, combine this with state.shoppie_aggro_levels > 0
+    bool aggro;         // setting this makes him angry, if it's shopkeeper you get 2 agrro points
+    bool was_hurt;      // also is set true if you set aggro to true, get's trigger even when whiping
     uint16_t padding1;
     uint32_t padding2;
 };
@@ -56,12 +56,12 @@ class NPC : public Monster
 class Ghost : public Monster
 {
   public:
-    uint16_t split_timer;       // for SMALL_HAPPY this is also the sequence timer of its various states
+    uint16_t split_timer; // for SMALL_HAPPY this is also the sequence timer of its various states
     uint8_t unknown1_timer;
     uint8_t unknown2;
     float velocity_multiplier;
-    uint16_t unknown3;          // layer change related
-    uint8_t ghost_behaviour;    // 0 = SMALL_ANGRY aka standard chasing ; 1 = SMALL_SURPRISED; 2 = SMALL_SAD; 3 = SMALL_HAPPY
+    uint16_t unknown3;       // layer change related
+    uint8_t ghost_behaviour; // 0 = SMALL_ANGRY aka standard chasing ; 1 = SMALL_SURPRISED; 2 = SMALL_SAD; 3 = SMALL_HAPPY
     // 4 and above = will move up and down, moving slightly more in one direction
     uint8_t unknown6;
     bool unknown7;
@@ -83,9 +83,9 @@ class Bat : public Monster
 class Jiangshi : public Monster
 {
   public:
-    uint8_t wait_timer;     // wait time between jumps
-    uint8_t jump_counter;   // only female aka assassin, when 0 will jump up into ceiling
-    bool on_ceiling;        // only female aka assassin
+    uint8_t wait_timer;   // wait time between jumps
+    uint8_t jump_counter; // only female aka assassin, when 0 will jump up into ceiling
+    bool on_ceiling;      // only female aka assassin
 };
 
 class Monkey : public Monster
@@ -144,11 +144,11 @@ class HangSpider : public Monster
 class Shopkeeper : public RoomOwner
 {
   public:
-    uint8_t name;                   // 0 - Ali, 1 - Bob, 2 - Comso ... and so one, anything above 28 is just random string, can crash the game
-    uint8_t shotgun_attack_delay;   // can't shot when the timer is running
-    uint8_t unknown3;               // accessed on stun/dmg? probably bool
+    uint8_t name;                 // 0 - Ali, 1 - Bob, 2 - Comso ... and so one, anything above 28 is just random string, can crash the game
+    uint8_t shotgun_attack_delay; // can't shot when the timer is running
+    uint8_t unknown3;             // accessed on stun/dmg? probably bool
     bool shop_owner;
-    bool unknown5a;                 // sometimes set to true
+    bool unknown5a; // sometimes set to true
     uint8_t padding1;
     uint8_t padding2;
     uint8_t padding3;
@@ -160,9 +160,9 @@ class Yang : public RoomOwner
     std::map<int64_t, int64_t> unknown2;
     uint8_t unknown4;
     uint8_t unknown5;
-    bool first_message_shown;    // I'm looking for turkeys, wanna help?
-    bool quest_incomplete;       // gets set to false when the quest is over (Yang dead or second turkey delivered)
-    bool special_message_shown;  // tusk palace, black market or one way door message shown
+    bool first_message_shown;   // I'm looking for turkeys, wanna help?
+    bool quest_incomplete;      // gets set to false when the quest is over (Yang dead or second turkey delivered)
+    bool special_message_shown; // tusk palace, black market or one way door message shown
     uint8_t padding4;
     uint8_t padding5;
     uint8_t padding6;
@@ -177,7 +177,7 @@ class Tun : public RoomOwner
     bool congrats_challenge; // congrats message shown after exiting a challenge
     bool murdered;           // when she disappears in a cloud of smoke
     bool shop_entered;
-    bool tiamat_encounter;   // if set to false, greets you with 'you've done well to reach this place'
+    bool tiamat_encounter; // if set to false, greets you with 'you've done well to reach this place'
     uint8_t padding;
 };
 
@@ -185,12 +185,12 @@ class Pet : public Monster
 {
   public:
     Entity* fx_button;
-    int32_t petting_by_uid;  // person whos petting it, only in the camp
-    int16_t yell_counter;    // counts up to 400 (6.6 sec); when 0 the pet yells out
+    int32_t petting_by_uid; // person whos petting it, only in the camp
+    int16_t yell_counter;   // counts up to 400 (6.6 sec); when 0 the pet yells out
     uint16_t unknown1;
     uint16_t unknown2;
-    int16_t func_timer;     // used when free running in the camp
-    int8_t active_state;    // -1 = sitting and yelling; 0 = either running, dead or picked up
+    int16_t func_timer;  // used when free running in the camp
+    int8_t active_state; // -1 = sitting and yelling; 0 = either running, dead or picked up
 };
 
 class Caveman : public WalkingMonster
@@ -217,9 +217,9 @@ class CavemanShopkeeper : public WalkingMonster
 class HornedLizard : public Monster
 {
   public:
-    int32_t eaten_uid;              // dungbeetle being eaten
-    uint8_t walk_pause_timer;       // alternates between walking and pausing when timer reaches zero
-    uint8_t attack_cooldown_timer;  // won't attack until timer reaches zero
+    int32_t eaten_uid;             // dungbeetle being eaten
+    uint8_t walk_pause_timer;      // alternates between walking and pausing when timer reaches zero
+    uint8_t attack_cooldown_timer; // won't attack until timer reaches zero
     uint8_t blood_squirt_timer;
     uint8_t unknown3;
     SoundPosition* sound_pos;
@@ -291,9 +291,9 @@ class FirebugUnchained : public Monster
 {
   public:
     SoundPosition* sound_pos;
-    float max_flight_height;  // increments when flying up, until it reaches 1.0, then bug flies down
-    uint32_t ai_timer;        // when reaches zero, decides whether to walk or fly or pause; doesn't reset when flying
-    uint32_t walking_timer;   // counts down only when walking; can't start flying unless this is at zero
+    float max_flight_height; // increments when flying up, until it reaches 1.0, then bug flies down
+    uint32_t ai_timer;       // when reaches zero, decides whether to walk or fly or pause; doesn't reset when flying
+    uint32_t walking_timer;  // counts down only when walking; can't start flying unless this is at zero
 };
 
 class Robot : public WalkingMonster
@@ -568,7 +568,7 @@ class Alien : public Monster
   public:
     uint8_t jump_timer; // when 0 it jumps
     uint8_t unknown1;
-    uint8_t unknown2;   // seen timer here once maybe? it's accessed right as he jumps from the ufo
+    uint8_t unknown2; // seen timer here once maybe? it's accessed right as he jumps from the ufo
 };
 
 class UFO : public Monster
@@ -630,7 +630,7 @@ class Olmite : public WalkingMonster
     bool in_stack;  // disables the attack, stun, lock's looking left flag between stack
     bool in_stack2; // is set to false couple frame after being detached from stack
     int8_t padding1;
-    int32_t on_top_uid; 
+    int32_t on_top_uid;
     float y_offset;                // same as y position when stacked
     uint8_t attack_cooldown_timer; // jump attacks can start when 0
     uint8_t unknown_countdown;
