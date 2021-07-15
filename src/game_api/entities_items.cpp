89c56534aa5184fb1b307bd6c6c52f5d7f1c0159
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2021-07-15 22:52:34 +0200

    improve the deploy function for the parachute powerup

--- src/game_api/entities_items.cpp
+++ src/game_api/entities_items.cpp
@@ -1,11 +1,20 @@
 #include "entities_items.hpp"
+#include "rpc.hpp"
 
 void ParachutePowerup::deploy()
 {
-    // this is not perfect solution as it won't work if you spawn_over parachutepowerup on player/alien
-    static auto alien = to_id("ENT_TYPE_MONS_ALIEN");
-    if (overlay->type->search_flags == 0b1 // MASK.PLAYER
-        || overlay->type->id == alien)
+    // this is not perfect solution as it won't work if you spawn_over parachutepowerup on player
+    auto state = State::get();
+    bool is_player = false;
+    for (int i = 0; i < MAX_PLAYERS; i++)
+    {
+        if (state.items()->player(i) == overlay)
+        {
+            is_player = true;
+            break;
+        }
+    }
+    if (is_player)
     {
         falltime_deploy = 0;
     }
@@ -13,9 +22,12 @@ void ParachutePowerup::deploy()
     {
         deployed = true;
         deployed2 = true;
-        flags = flags ^ 0b1001;           // unset 1 & 4
-        more_flags = more_flags | 0b1000; // set 4
-        y = 0.5;
+        if ((flags & 0b1001) == 0b1001)
+            flags = flags ^ 0b1001; // unset 1 & 4
+        if ((more_flags & 0b1000) != 0b1000)
+            more_flags = more_flags | 0b1000; // set 4
+        if (y == 0.0)
+            y = 0.5;
         offsety = 0.115;
         hitboxy = 0.285;
         duckmask = 2143944961; // hehe funny number
