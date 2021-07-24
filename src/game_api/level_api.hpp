6e7eaee14a93d7022a363e45778eb37273aba3ac
zappatic <github@zappatic.net>
2021-07-24 21:03:22 +0200

    added some levelgen virtuals

--- src/game_api/level_api.hpp
+++ src/game_api/level_api.hpp
@@ -79,9 +79,8 @@ struct DoorCoords
     float door2_y;
 };
 
-struct ThemeInfo
+class ThemeInfo
 {
-    void** __vftable;
     bool unknown1; // gets set to false for the jungle and temple theme in the cosmic ocean
     bool unknown2;
     uint8_t padding1;
@@ -90,6 +89,68 @@ struct ThemeInfo
     ThemeInfo* sub_theme;
     uint32_t unknown3;
     uint32_t unknown4;
+
+    virtual ~ThemeInfo();
+    virtual bool get_unknown1() = 0;
+
+    // dwelling,tidepool: unset levelgen.flags.flag12
+    // jungle,volcana.olmec,icecaves,neobab,cog,duat,abzu,tiamat,eggplant,hundun,basecamp,arena: nop
+    // temple: unset levelgen.flags.flag10 + calculate chance of grasshopper critter spawn
+    // sunken: unset levelgen.flags.flag9, 10, 17, 18 + if state.level == 1 -> unset flag 11
+    // cosmic: calls same virtual on its sub_theme
+    virtual void initialize_flags() = 0;
+
+    // does random calculations and calls function to determine the start room in most themes
+    virtual void initialize_levelgen() = 0;
+
+    // most themes call the same function, some check whether they are in CO
+    virtual void unknown_v4() = 0;
+
+    virtual void unknown_v5() = 0;
+
+    // dwelling: does stuff when level == 4 or udjat present
+    // jungle: when black market present
+    // volcana: when drill present
+    // touches the rooms and sometimes the meta info about the rooms
+    virtual void handle_level_specialities() = 0;
+
+    // can't trigger, dwelling (quillback) and abzu do something special (arena just returns)
+    virtual void unknown_v7() = 0;
+
+    // does something depending on levelgen.data.unknown7
+    virtual void unknown_v8() = 0;
+
+    virtual void insert_shopkeeper_vault() = 0;
+
+    virtual void insert_coffin() = 0;
+
+    // metal clanking and air of oppression
+    virtual void set_theme_specific_level_feeling() = 0;
+
+    // disable this and only the player is spawned in the level
+    virtual void populate_level() = 0;
+
+    virtual void add_level_bordertiles() = 0;
+
+    // volcana: checks if state.coffin_contents = cocovondiamonds -> chooses one of the four coffins in vlad's castle at random to put her in
+    // tidepool: spawns impostor lake, some door and a litwalltorch, ...
+    // neobab: assigns the correct animation_frame to all the ushabtis
+    // co, arena: forwards to the same virtual of the sub_theme
+    // abzu, tiamat: spawns impostor lake
+    virtual void post_process_level() = 0;
+
+    // quillback: adds butterflies and snails + configures the door to go to the correct theme/level
+    // olmec: same thing but crabs and grasshoppers
+    // temple: spawns COG door
+    // co, arena: forwards to the same virtual of the sub_theme
+    // other themes also do stuff, but nothing seemingly important
+    virtual void post_process_exitdoors() = 0;
+
+    // pleasure palace: applies correct texture to ladders and ladder platforms
+    // sunken city: randomly adds ENT_TYPE_DECORATION_SUNKEN_BRIDGE between entities (the slimy bridges)
+    // hundun: calls sunken city virtual, so has slimy bridges
+    // co: does nothing, so no slimy bridges in CO sunken city levels! (forgotten/bug?)
+    virtual void post_process_entities() = 0;
 };
 static_assert(sizeof(ThemeInfo) == 0x20);
 
@@ -162,9 +223,7 @@ struct LevelGenSystem
     DoorCoords* exit_doors_locations;
     void* unknown37;
     void* unknown38;
-    uint16_t flags;
-    int8_t unknown40;
-    uint8_t unknown41;
+    uint32_t flags;
     uint8_t unknown42;
     uint8_t unknown43;
     uint8_t unknown44;
