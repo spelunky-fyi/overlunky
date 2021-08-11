a742bb4e221b8ffd524fd666f784913c7e79745e
Malacath <h.baensch.92@gmail.com>
2021-08-11 12:33:12 +0200

    Fix a startup crash when fetching textures

--- src/game_api/texture.cpp
+++ src/game_api/texture.cpp
@@ -2,19 +2,16 @@
 
 #include "memory.hpp"
 
-size_t textures_ptr()
-{
-    ONCE(size_t)
-    {
-        auto mem = Memory::get();
-        res = mem.at_exe(decode_pc(
-                  mem.exe(), find_inst(mem.exe(), "\x48\x8d"s, find_inst(mem.exe(), "\x75\x06\x41\x8b\xec\x41\x8b\xf7"s, mem.after_bundle)))) -
-              8;
-        return res;
-    }
-}
-
 Textures* get_textures()
 {
-    return (Textures*)textures_ptr();
+    static Textures* textures_ptr = (Textures*)[]()
+    {
+        auto mem = Memory::get();
+        size_t off = find_inst(mem.exe(), "\x75\x06\x41\x8b\xec\x41\x8b\xf7"s, mem.after_bundle);
+        off = find_inst(mem.exe(), "\x48\x8d"s, off);
+        off = mem.at_exe(decode_pc(mem.exe(), off) - 8);
+        return off;
+    }
+    ();
+    return textures_ptr;
 }
