74fb68457047e52f0bacc47d9c76de09144c5a9c
Dregu <dregu@dreg.us>
2021-08-13 22:30:24 +0300

    rando2: bump version

--- examples/randomizer2.lua
+++ examples/randomizer2.lua
@@ -1,6 +1,8 @@
 meta.name = "Randomizer Two"
-meta.description = "Second incarnation of The Randomizer with new API shenannigans. Most familiar things from 1.2 are still there, but better! Progression is changed though, shops are random, level gen is crazy, chain item stuff, multiple endings, secrets... I can't possibly test all of this so fingers crossed it doesn't crash a lot."
-meta.version = "2.0-RC3"
+meta.description = [[Fair, balanced, beginner friendly... These are not words I would use to describe The Randomizer. Fun though? Abso-hecking-lutely.
+    
+Second incarnation of The Randomizer with new API shenannigans. Most familiar things from 1.2 are still there, but better! Progression is changed though, shops are random, level gen is crazy, chain item stuff, multiple endings, secrets... I can't possibly test all of this so fingers crossed it doesn't crash a lot.]]
+meta.version = "2.0"
 meta.author = "Dregu"
 
 local function get_chance(min, max)
