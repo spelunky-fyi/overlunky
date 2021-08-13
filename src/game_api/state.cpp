e556184e8c58dc4912083c92a2dd23618fc496ca
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2021-08-13 21:03:36 +0200

    change set_correct_ushabti_af calculation so it makes more sense when looking at it

--- src/game_api/state.cpp
+++ src/game_api/state.cpp
@@ -11,7 +11,7 @@ uint16_t StateMemory::get_correct_ushabti_af()
 }
 void StateMemory::set_correct_ushabti_af(uint16_t animation_frame)
 {
-    correct_ushabti = static_cast<uint8_t>(animation_frame - 2 * (animation_frame / 12));
+    correct_ushabti = static_cast<uint8_t>(animation_frame % 12 + (animation_frame / 12) * 10);
 }
 
 size_t get_dark()
