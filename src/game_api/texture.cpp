f551716691d69f521d6e048525f59f0c101f7679
zappatic <github@zappatic.net>
2021-09-21 15:11:03 +0200

    Pattern for get_textures() TextureDB

--- src/game_api/texture.cpp
+++ src/game_api/texture.cpp
@@ -6,10 +6,12 @@ Textures* get_textures()
 {
     static Textures* textures_ptr = (Textures*)[]()
     {
+        // Rev.Eng.: Look up string reference to "Data/Textures/", at the beginning of this function
+        // there will be a pointer to the TextureDB start
         auto mem = Memory::get();
-        size_t off = find_inst(mem.exe(), "\x75\x06\x41\x8b\xec\x41\x8b\xf7"s, mem.after_bundle);
-        off = find_inst(mem.exe(), "\x48\x8d"s, off);
-        off = mem.at_exe(decode_pc(mem.exe(), off) - 8);
+        size_t off = find_inst(mem.exe(), "\x4C\x89\xC6\x41\x89\xCF\x8B\x1D"s, mem.after_bundle);
+        // Add 8 because this particular pattern points to the size qword preceding the actual table
+        off = mem.at_exe(decode_pc(mem.exe(), off + 6, 2)) + 8;
         return off;
     }
     ();
