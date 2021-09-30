4efe6ac86974f730cda9caa73c1bb47327410d45
Malacath <h.baensch.92@gmail.com>
2021-09-30 08:55:50 +0200

    Fix misuse of malloc/free patterns

--- src/game_api/game_allocator.cpp
+++ src/game_api/game_allocator.cpp
@@ -7,11 +7,11 @@ using FreeFun = decltype(game_free);
 
 void* game_malloc(std::size_t size)
 {
-    static MallocFun* _malloc = (MallocFun*)get_address("game_malloc"sv);
+    static MallocFun* _malloc = *(MallocFun**)get_address("game_malloc"sv);
     return _malloc(size);
 }
 void game_free(void* mem)
 {
-    static FreeFun* _free = (FreeFun*)get_address("game_free");
+    static FreeFun* _free = *(FreeFun**)get_address("game_free"sv);
     _free(mem);
 }
