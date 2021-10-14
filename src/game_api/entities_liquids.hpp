41f34fb7c86cf2a492b072cd368f6603b8bda01f
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2021-10-14 11:56:57 +0200

    fix liquid type

--- src/game_api/entities_liquids.hpp
+++ src/game_api/entities_liquids.hpp
@@ -8,11 +8,15 @@ class Liquid : public Entity
 {
   public:
     Entity* fx_surface;
-    size_t unknown;
+    float x_pos;
+    float y_pos;
+    size_t unknown; // MysteryLiquidPointer1 in plugin
     uint16_t unknown_readonly1;
     uint16_t unknown_readonly2;
-    uint16_t unknown_timer1;
-    uint16_t unknown_timer2;
+    uint8_t unknown_timer1;
+    uint8_t unknown_timer2;
+    uint8_t unknown_timer3;
+    uint8_t unk21; //probably padding
 };
 
 class Lava : public Liquid
