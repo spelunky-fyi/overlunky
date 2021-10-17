a08dfcdcf79eaf92f375911c13d07c2e2004436f
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2021-10-17 15:20:58 +0200

    fix silliness

--- src/game_api/strings.hpp
+++ src/game_api/strings.hpp
@@ -9,5 +9,5 @@ void strings_init();
 const char16_t** get_strings_table();
 STRINGID hash_to_stringid(uint32_t hash);
 const char16_t* get_string(STRINGID string_id);
-void change_string(STRINGID string_id, const std::u16string str);
-STRINGID add_string(const std::u16string str);
+void change_string(STRINGID string_id, std::u16string str);
+STRINGID add_string(std::u16string str);
