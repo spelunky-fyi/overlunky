9a4c7de691a386285d4e5210b442721ffd4c34b4
Dregu <dregu@dreg.us>
2021-12-20 23:02:14 +0200

    format...

--- src/injected/ui.cpp
+++ src/injected/ui.cpp
@@ -258,11 +258,11 @@ LARGE_INTEGER g_speedhack_current;
 LARGE_INTEGER g_speedhack_fake;
 PVOID g_oldqpc;
 
-#define PtrFromRva( base, rva ) ( ( ( PBYTE ) base ) + rva )
+#define PtrFromRva(base, rva) (((PBYTE)base) + rva)
 
 // I didn't write this one, I just found it in the shady parts of the internet
 // This could probably be done with detours and speedhack should be part of the api anyway...
-BOOL HookIAT(const char *szModuleName, const char *szFuncName, PVOID pNewFunc, PVOID *pOldFunc)
+BOOL HookIAT(const char* szModuleName, const char* szFuncName, PVOID pNewFunc, PVOID* pOldFunc)
 {
     PIMAGE_DOS_HEADER pDosHeader = (PIMAGE_DOS_HEADER)GetModuleHandle(NULL);
     PIMAGE_NT_HEADERS pNtHeader = (PIMAGE_NT_HEADERS)PtrFromRva(pDosHeader, pDosHeader->e_lfanew);
@@ -274,54 +274,54 @@ BOOL HookIAT(const char *szModuleName, const char *szFuncName, PVOID pNewFunc, P
     // Grab a pointer to the import data directory
     PIMAGE_IMPORT_DESCRIPTOR pImportDescriptor = (PIMAGE_IMPORT_DESCRIPTOR)PtrFromRva(pDosHeader, pNtHeader->OptionalHeader.DataDirectory[IMAGE_DIRECTORY_ENTRY_IMPORT].VirtualAddress);
 
-        for (UINT uIndex = 0; pImportDescriptor[uIndex].Characteristics != 0; uIndex++)
-        {
-            char *szDllName = (char*)PtrFromRva(pDosHeader, pImportDescriptor[uIndex].Name);
+    for (UINT uIndex = 0; pImportDescriptor[uIndex].Characteristics != 0; uIndex++)
+    {
+        char* szDllName = (char*)PtrFromRva(pDosHeader, pImportDescriptor[uIndex].Name);
 
-            // Is this our module?
-            if (_strcmpi(szDllName, szModuleName) != 0)
+        // Is this our module?
+        if (_strcmpi(szDllName, szModuleName) != 0)
+            continue;
+
+        if (!pImportDescriptor[uIndex].FirstThunk || !pImportDescriptor[uIndex].OriginalFirstThunk)
+            return FALSE;
+
+        PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)PtrFromRva(pDosHeader, pImportDescriptor[uIndex].FirstThunk);
+        PIMAGE_THUNK_DATA pOrigThunk = (PIMAGE_THUNK_DATA)PtrFromRva(pDosHeader, pImportDescriptor[uIndex].OriginalFirstThunk);
+
+        for (; pOrigThunk->u1.Function != NULL; pOrigThunk++, pThunk++)
+        {
+            // We can't process ordinal imports just named
+            if (pOrigThunk->u1.Ordinal & IMAGE_ORDINAL_FLAG)
                 continue;
 
-            if (!pImportDescriptor[uIndex].FirstThunk || !pImportDescriptor[uIndex].OriginalFirstThunk)
+            PIMAGE_IMPORT_BY_NAME import = (PIMAGE_IMPORT_BY_NAME)PtrFromRva(pDosHeader, pOrigThunk->u1.AddressOfData);
+
+            // Is this our function?
+            if (_strcmpi(szFuncName, (char*)import->Name) != 0)
+                continue;
+
+            DWORD dwJunk = 0;
+            MEMORY_BASIC_INFORMATION mbi;
+
+            // Make the memory section writable
+            VirtualQuery(pThunk, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
+            if (!VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &mbi.Protect))
                 return FALSE;
 
-            PIMAGE_THUNK_DATA pThunk = (PIMAGE_THUNK_DATA)PtrFromRva(pDosHeader, pImportDescriptor[uIndex].FirstThunk);
-            PIMAGE_THUNK_DATA pOrigThunk = (PIMAGE_THUNK_DATA)PtrFromRva(pDosHeader, pImportDescriptor[uIndex].OriginalFirstThunk);
-
-            for (; pOrigThunk->u1.Function != NULL; pOrigThunk++, pThunk++)
-            {
-                // We can't process ordinal imports just named
-                if (pOrigThunk->u1.Ordinal & IMAGE_ORDINAL_FLAG)
-                    continue;
-
-                PIMAGE_IMPORT_BY_NAME import = (PIMAGE_IMPORT_BY_NAME)PtrFromRva(pDosHeader, pOrigThunk->u1.AddressOfData);
-
-                // Is this our function?
-                if (_strcmpi(szFuncName, (char*)import->Name) != 0)
-                    continue;
-
-                DWORD dwJunk = 0;
-                MEMORY_BASIC_INFORMATION mbi;
-
-                // Make the memory section writable
-                VirtualQuery(pThunk, &mbi, sizeof(MEMORY_BASIC_INFORMATION));
-                if (!VirtualProtect(mbi.BaseAddress, mbi.RegionSize, PAGE_EXECUTE_READWRITE, &mbi.Protect))
-                    return FALSE;
-
-                // Save the old pointer
-                *pOldFunc = (PVOID*)(DWORD_PTR)pThunk->u1.Function;
+            // Save the old pointer
+            *pOldFunc = (PVOID*)(DWORD_PTR)pThunk->u1.Function;
 
 // Write the new pointer based on CPU type
 #ifdef _WIN64
-                pThunk->u1.Function = (ULONGLONG)(DWORD_PTR)pNewFunc;
+            pThunk->u1.Function = (ULONGLONG)(DWORD_PTR)pNewFunc;
 #else
-                pThunk->u1.Function = (DWORD)(DWORD_PTR)pNewFunc;
+            pThunk->u1.Function = (DWORD)(DWORD_PTR)pNewFunc;
 #endif
 
-                if (VirtualProtect(mbi.BaseAddress, mbi.RegionSize, mbi.Protect, &dwJunk))
-                    return TRUE;
-            }
+            if (VirtualProtect(mbi.BaseAddress, mbi.RegionSize, mbi.Protect, &dwJunk))
+                return TRUE;
         }
+    }
     return FALSE;
 }
 
@@ -330,7 +330,7 @@ bool __stdcall QueryPerformanceCounterHook(LARGE_INTEGER* counter)
     QueryPerformanceCounter(&g_speedhack_current);
     g_speedhack_fake.QuadPart += (long long)((g_speedhack_current.QuadPart - g_speedhack_prev.QuadPart) * g_speedhack_multiplier);
     g_speedhack_prev = g_speedhack_current;
-    *counter =  g_speedhack_fake;
+    *counter = g_speedhack_fake;
     return true;
 }
 
