f5f715cd5ac191e88f2b1e772ae5542a678aa146
Malacath <h.baensch.92@gmail.com>
2021-12-21 16:08:16 +0100

    Add enable_steam_achievements

--- src/game_api/steam_api.cpp
+++ src/game_api/steam_api.cpp
@@ -3,23 +3,39 @@
 #include "search.hpp"
 #include "vtable_hook.hpp"
 
-void disable_steam_achievements()
+class ISteamUserStats* get_steam_user_stats()
 {
-    class ISteamUserStats;
     using GetSteamUserStatsFun = void(ISteamUserStats**);
-    GetSteamUserStatsFun* get_steam_user_stats = *(GetSteamUserStatsFun**)get_address("get_steam_user_stats");
+    static GetSteamUserStatsFun* get_steam_user_stats_impl = *(GetSteamUserStatsFun**)get_address("get_steam_user_stats");
 
     ISteamUserStats* steam_user_stats{nullptr};
-    get_steam_user_stats(&steam_user_stats);
-    if (steam_user_stats != nullptr)
+    get_steam_user_stats_impl(&steam_user_stats);
+    return steam_user_stats;
+}
+
+void enable_steam_achievements()
+{
+    ISteamUserStats* steam_user_stats = get_steam_user_stats();
+    if (steam_user_stats != nullptr && get_hook_function((void***)steam_user_stats, 0x7))
     {
+        unregister_hook_function((void***)steam_user_stats, 0x7);
+    }
+}
+
+void disable_steam_achievements()
+{
+    ISteamUserStats* steam_user_stats = get_steam_user_stats();
+    if (steam_user_stats != nullptr && !get_hook_function((void***)steam_user_stats, 0x7))
+    {
+        constexpr auto nop_set_achievement = [](ISteamUserStats*, const char*)
+        {
+            return true;
+        };
+
         using ISteamUserStats_SetAchievement = bool(ISteamUserStats*, const char*);
-        hook_vtable_no_dtor<ISteamUserStats_SetAchievement>(
-            steam_user_stats,
-            [](ISteamUserStats*, const char*, ISteamUserStats_SetAchievement*)
-            {
-                return true;
-            },
-            0x7);
+        register_hook_function(
+            (void***)steam_user_stats,
+            0x7,
+            (ISteamUserStats_SetAchievement*)nop_set_achievement);
     }
 }
--- src/game_api/steam_api.hpp
+++ src/game_api/steam_api.hpp
@@ -1,3 +1,4 @@
 #pragma once
 
+void enable_steam_achievements();
 void disable_steam_achievements();
