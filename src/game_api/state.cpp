abcc3617067964d2871c1ef3a7f146e50c416f9c
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2022-01-24 11:55:35 +0100

    write_load_opt again

--- src/game_api/state.cpp
+++ src/game_api/state.cpp
@@ -26,15 +26,7 @@ inline bool& get_is_init()
 
 void do_write_load_opt()
 {
-    size_t write_load_addr = get_address("write_load_opt");
-    if (write_load_addr > 0ull)
-    {
-        write_mem_prot(write_load_addr, "\x90\x90"sv, true);
-    }
-    else
-    {
-        assert(get_address("write_load_opt") > 0ull);
-    }
+    write_mem_prot(get_address("write_load_opt"), "\x90\x90"sv, true);
 }
 bool& get_write_load_opt()
 {
