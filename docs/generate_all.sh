555626e2662b4aebe60f719aa435a5943f355cf7
Dregu <dregu@dreg.us>
2022-02-20 02:57:10 +0200

    fix docs workflow

--- docs/generate_all.sh
+++ docs/generate_all.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 python generate.py
 ./generate_slate.sh
+git config --local user.email "41898282+github-actions[bot]@users.noreply.github.com"
+git config --local user.name "github-actions[bot]"
 git add fonts images javascripts stylesheets
 git commit -am "update slate[no ci]"
