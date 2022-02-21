25eafce33c944782a5529b270f237f30004b0175
Dregu <dregu@dreg.us>
2022-02-21 05:36:54 +0200

    fix meta docs

---
+++ docs/examples/meta.lua
@@ -0,0 +1,12 @@
+meta.name = "Awesome Mod"
+meta.version = "1.0"
+meta.author = "You"
+meta.description = [[
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Ut at nulla porttitor, lobortis magna at, tempus dolor. Cras non ligula tellus. Duis tincidunt sodales velit et ornare. Mauris eu sapien finibus dolor dictum malesuada in non elit.
+
+    Aenean luctus leo ut diam ornare viverra. Nunc condimentum interdum elit, quis porttitor quam finibus ac. Nam mattis, lectus commodo placerat tristique, justo justo congue dui, sed sodales nunc sem ut neque.
+]]
+-- set this to enable unsafe mode
+--meta.unsafe = true
+
+-- rest of your mod goes here
