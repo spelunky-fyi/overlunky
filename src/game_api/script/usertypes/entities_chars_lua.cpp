c2528bda3354092914b253e2d52f7d5b9ae08086
Cosine256 <4402061+Cosine256@users.noreply.github.com>
2022-12-09 12:33:37 -0700

    Expose player coyote timer

--- src/game_api/script/usertypes/entities_chars_lua.cpp
+++ src/game_api/script/usertypes/entities_chars_lua.cpp
@@ -113,6 +113,7 @@ void register_usertypes(sol::state& lua)
     player_type["ai"] = &Player::ai;
     player_type["input"] = &Player::input_ptr;
     player_type["basecamp_button_entity"] = &Player::basecamp_button_entity;
+    player_type["coyote_timer"] = &Player::coyote_timer;
     player_type["set_jetpack_fuel"] = &Player::set_jetpack_fuel;
     player_type["kapala_blood_amount"] = &Player::kapala_blood_amount;
     player_type["get_name"] = &Player::get_name;
