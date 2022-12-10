1d2b68d3232d81c9f7712397304f5ab9ea2810aa
Cosine256 <4402061+Cosine256@users.noreply.github.com>
2022-12-09 18:49:29 -0700

    Fix signed/unsigned mismatch build error

--- src/injected/ui.cpp
+++ src/injected/ui.cpp
@@ -7091,7 +7091,7 @@ void imgui_draw()
     auto base = ImGui::GetMainViewport();
     ImGuiContext& g = *GImGui;
 
-    if (get_setting(GAME_SETTING::WINDOW_MODE) == 0)
+    if (get_setting(GAME_SETTING::WINDOW_MODE) == 0u)
         ImGui::GetIO().ConfigFlags &= ~ImGuiConfigFlags_ViewportsEnable;
     else if (options["multi_viewports"])
         ImGui::GetIO().ConfigFlags |= ImGuiConfigFlags_ViewportsEnable;
