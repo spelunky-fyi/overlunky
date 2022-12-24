cd0646354490d0cb7730bd37d923b8e72c909f41
Malacath <h.baensch.92@gmail.com>
2022-12-24 15:14:22 +0100

    Get rid of bad hardcoded value

--- src/game_api/containers/custom_allocator.cpp
+++ src/game_api/containers/custom_allocator.cpp
@@ -12,12 +12,12 @@ using CustomFreeFun = void*(void*, void*);
 void* custom_malloc(std::size_t size)
 {
     static CustomMallocFun* _malloc = (CustomMallocFun*)get_address("custom_malloc"sv);
-    static void* _alloc_base = OnHeapPointer<void>(memory_read<uint64_t>(Memory::get().at_exe(0x22dd96b8))).decode();
+    static void* _alloc_base = OnHeapPointer<void>(*(size_t*)get_address("malloc_base"sv)).decode();
     return _malloc(_alloc_base, size);
 }
 void custom_free(void* mem)
 {
     static CustomFreeFun* _free = (CustomFreeFun*)get_address("custom_free"sv);
-    static void* _alloc_base = OnHeapPointer<void>(memory_read<uint64_t>(Memory::get().at_exe(0x22dd96b8))).decode();
+    static void* _alloc_base = OnHeapPointer<void>(*(size_t*)get_address("malloc_base"sv)).decode();
     _free(_alloc_base, mem);
 }
