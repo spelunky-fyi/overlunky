484b336f86493ca5fd1991f8c3e6e41864d8cf22
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2022-12-26 13:30:04 +0100

    make the draw rect/triangle not filled a thing, still WIP

--- src/game_api/script/usertypes/vanilla_render_lua.cpp
+++ src/game_api/script/usertypes/vanilla_render_lua.cpp
@@ -40,13 +40,33 @@ void VanillaRenderContext::draw_screen_texture(TEXTURE texture_id, uint8_t row, 
     draw_screen_texture(texture_id, row, column, Quad(rect), std::move(color));
 }
 
+Quad& convert_ratio(Quad& quad, bool to_screen)
+{
+    constexpr float ratio = 16.0f / 9.0f;
+    constexpr float inverse_ratio = 9.0f / 16.0f;
+    if (to_screen)
+    {
+        quad.bottom_left_x *= inverse_ratio;
+        quad.bottom_right_x *= inverse_ratio;
+        quad.top_left_x *= inverse_ratio;
+        quad.top_right_x *= inverse_ratio;
+    }
+    else
+    {
+        quad.bottom_left_x *= ratio;
+        quad.bottom_right_x *= ratio;
+        quad.top_left_x *= ratio;
+        quad.top_right_x *= ratio;
+    }
+    return quad;
+}
+
 void VanillaRenderContext::draw_screen_texture(TEXTURE texture_id, uint8_t row, uint8_t column, const AABB& rect, Color color, float angle, float px, float py)
 {
     Quad dest{rect};
     if (angle != 0)
     {
         constexpr float ratio = 16.0f / 9.0f;
-        constexpr float inverse_ratio = 9.0f / 16.0f;
 
         // fix ratio to 1/1 to properly rotate the coordinates
         const AABB new_rect{rect.left * ratio, rect.top, rect.right * ratio, rect.bottom};
@@ -54,16 +74,12 @@ void VanillaRenderContext::draw_screen_texture(TEXTURE texture_id, uint8_t row, 
         auto pivot = new_rect.center();
         if (px != 0 || py != 0)
         {
-            pivot.first += abs(pivot.first - new_rect.left) * px;
-            pivot.second += abs(pivot.second - new_rect.bottom) * py;
+            pivot.first += std::abs(pivot.first - new_rect.left) * px;
+            pivot.second += std::abs(pivot.second - new_rect.bottom) * py;
         }
 
         dest.rotate(angle, pivot.first, pivot.second);
-        // bring back to the 16/9
-        dest.bottom_left_x *= inverse_ratio;
-        dest.bottom_right_x *= inverse_ratio;
-        dest.top_left_x *= inverse_ratio;
-        dest.top_right_x *= inverse_ratio;
+        convert_ratio(dest, true);
     }
     draw_screen_texture(texture_id, row, column, dest, std::move(color));
 }
@@ -107,13 +123,77 @@ void VanillaRenderContext::draw_screen_texture(TEXTURE texture_id, const Quad& s
     RenderAPI::get().draw_screen_texture(texture, source, quad, std::move(color), 0x29);
 }
 
+// get intersection point of two lines
+Vec2 intersection(const Vec2 A, const Vec2 B, const Vec2 C, const Vec2 D)
+{
+    float a = B.y - A.y;
+    float b = A.x - B.x;
+    float c = a * (A.x) + b * (A.y);
+    // Line CD represented as a2x + b2y = c2
+    float a1 = D.y - C.y;
+    float b1 = C.x - D.x;
+    float c1 = a1 * (C.x) + b1 * (C.y);
+
+    float det = a * b1 - a1 * b;
+
+    if (det == 0)
+        return {};
+    return Vec2{(b1 * c - b * c1) / det, (a * c1 - a1 * c) / det};
+}
+// get a Quad to out fill the corner between two
+Quad get_corner_quad(const Quad& line1, const Quad& line2, const Vec2 inner_corner)
+{
+    // TODO: would probably be better if it calculated the inner corner as well
+    // then decide which one is the inner and which the outer, maybe even merge this with the fix_overlap
+
+    // assumes to use `bottom` to get the outer corner
+    Vec2 A{line1.bottom_left_x, line1.bottom_left_y};
+    Vec2 B{line1.bottom_right_x, line1.bottom_right_y};
+
+    Vec2 C{line2.bottom_right_x, line2.bottom_right_y};
+    Vec2 D{line2.bottom_left_x, line2.bottom_left_y};
+
+    Vec2 outer_corner = intersection(A, B, C, D);
+
+    return Quad{B, inner_corner, D, outer_corner};
+};
+// convert two points into Quad (rectangle) with given `thickness` (height)
+Quad get_line_quad(const Vec2 A, const Vec2 B, float thickness)
+{
+    thickness *= .001f;
+    float axis_AB_angle = std::atan2((B.y - A.y), (B.x) - (A.x));
+    float hypotenuse = (float)std::sqrt(std::pow(B.x - A.x, 2) + std::pow(B.y - A.y, 2));
+
+    // make it straight and then rotate because i'm stupid
+    Quad dest{AABB{A.x, A.y + thickness * 0.5f, (A.x + hypotenuse), A.y - thickness * 0.5f}};
+    dest.rotate(axis_AB_angle, A.x, A.y);
+    return std::move(dest);
+};
+// removes the overlap of two lines (very specific use case when the lines are guaranteed to overlap)
+Vec2 fix_overlap(Quad& line1, Quad& line2)
+{
+    // assumes to use `top` to get the inner corner
+    Vec2 A{line1.top_right_x, line1.top_right_y};
+    Vec2 B{line1.top_left_x, line1.top_left_y};
+
+    Vec2 C{line2.top_right_x, line2.top_right_y};
+    Vec2 D{line2.top_left_x, line2.top_left_y};
+
+    Vec2 cross_point = intersection(A, B, C, D);
+
+    line1.top_right_x = cross_point.x;
+    line1.top_right_y = cross_point.y;
+    line2.top_left_x = cross_point.x;
+    line2.top_left_y = cross_point.y;
+    return cross_point;
+}
+
 void VanillaRenderContext::draw_screen_rect(const AABB& rect, Color color, bool filled, float thickness, float angle, float px, float py)
 {
     Quad dest{rect};
     if (angle != 0)
     {
         constexpr float ratio = 16.0f / 9.0f;
-        constexpr float inverse_ratio = 9.0f / 16.0f;
 
         // fix ratio to 1/1 to properly rotate the coordinates
         const AABB new_rect{rect.left * ratio, rect.top, rect.right * ratio, rect.bottom};
@@ -121,16 +201,11 @@ void VanillaRenderContext::draw_screen_rect(const AABB& rect, Color color, bool 
         auto pivot = new_rect.center();
         if (px != 0 || py != 0)
         {
-            pivot.first += abs(pivot.first - new_rect.left) * px;
-            pivot.second += abs(pivot.second - new_rect.bottom) * py;
+            pivot.first += std::abs(pivot.first - new_rect.left) * px;
+            pivot.second += std::abs(pivot.second - new_rect.bottom) * py;
         }
-
         dest.rotate(angle, pivot.first, pivot.second);
-        // bring back to the 16/9
-        dest.bottom_left_x *= inverse_ratio;
-        dest.bottom_right_x *= inverse_ratio;
-        dest.top_left_x *= inverse_ratio;
-        dest.top_right_x *= inverse_ratio;
+        convert_ratio(dest, true);
     }
     draw_screen_rect(dest, std::move(color), filled, thickness);
 }
@@ -139,18 +214,39 @@ void VanillaRenderContext::draw_screen_rect(const Quad& dest, Color color, bool 
 {
     if (filled)
     {
-        auto texture = get_texture(0);                                                    // any texture works
-        Quad src = Quad{0, 0, 0, 0, 0, 0, 0, 0};                                          // dunno, doesn't need much
-        RenderAPI::get().draw_screen_texture(texture, src, dest, std::move(color), 0x27); // 0x27 funky shader, 2C also works
+        auto texture = get_texture(0);                                                       // any texture works
+        RenderAPI::get().draw_screen_texture(texture, Quad{}, dest, std::move(color), 0x27); // 0x27 funky shader, 2C also works
     }
     else if (thickness)
     {
+        Quad copy{dest};
         // bottom_left, bottom_right, top_right, top_left
-        std::tuple<Vec2, Vec2, Vec2, Vec2> corners = dest;
-        draw_screen_line(std::get<0>(corners), std::get<1>(corners), std::move(color), thickness);
-        draw_screen_line(std::get<1>(corners), std::get<2>(corners), std::move(color), thickness);
-        draw_screen_line(std::get<2>(corners), std::get<3>(corners), std::move(color), thickness);
-        draw_screen_line(std::get<3>(corners), std::get<0>(corners), std::move(color), thickness);
+        std::tuple<Vec2, Vec2, Vec2, Vec2> corners = convert_ratio(copy, false);
+
+        Quad line_ab = get_line_quad(std::get<0>(corners), std::get<1>(corners), thickness);
+        Quad line_bc = get_line_quad(std::get<1>(corners), std::get<2>(corners), thickness);
+        Quad line_cd = get_line_quad(std::get<2>(corners), std::get<3>(corners), thickness);
+        Quad line_da = get_line_quad(std::get<3>(corners), std::get<0>(corners), thickness);
+
+        Vec2 cross_point1 = fix_overlap(line_ab, line_bc);
+        Vec2 cross_point2 = fix_overlap(line_bc, line_cd);
+        Vec2 cross_point3 = fix_overlap(line_cd, line_da);
+        Vec2 cross_point4 = fix_overlap(line_da, line_ab);
+
+        Quad corner_abc = get_corner_quad(line_ab, line_bc, cross_point1);
+        Quad corner_bcd = get_corner_quad(line_bc, line_cd, cross_point2);
+        Quad corner_cda = get_corner_quad(line_cd, line_da, cross_point3);
+        Quad corner_dac = get_corner_quad(line_da, line_ab, cross_point4);
+
+        draw_screen_rect(convert_ratio(line_ab, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(line_bc, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(line_cd, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(line_da, true), color, true, .0f);
+
+        draw_screen_rect(convert_ratio(corner_abc, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(corner_bcd, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(corner_cda, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(corner_dac, true), color, true, .0f);
     }
 }
 
@@ -162,23 +258,40 @@ void VanillaRenderContext::draw_screen_triangle(const Triangle& triangle, Color 
     }
     else if (thickness)
     {
-        draw_screen_line(triangle.A, triangle.B, std::move(color), thickness);
-        draw_screen_line(triangle.B, triangle.C, std::move(color), thickness);
-        draw_screen_line(triangle.C, triangle.A, std::move(color), thickness);
+        constexpr float ratio = 16.0f / 9.0f;
+
+        Triangle new_triangle{triangle.A.x * ratio, triangle.A.y, triangle.B.x * ratio, triangle.B.y, triangle.C.x * ratio, triangle.C.y};
+
+        Quad line_ab = get_line_quad(new_triangle.A, new_triangle.B, thickness);
+        Quad line_bc = get_line_quad(new_triangle.B, new_triangle.C, thickness);
+        Quad line_ca = get_line_quad(new_triangle.C, new_triangle.A, thickness);
+
+        Vec2 cross_point1 = fix_overlap(line_ab, line_bc);
+        Vec2 cross_point2 = fix_overlap(line_bc, line_ca);
+        Vec2 cross_point3 = fix_overlap(line_ca, line_ab);
+
+        Quad corner_abc = get_corner_quad(line_ab, line_bc, cross_point1);
+        Quad corner_bca = get_corner_quad(line_bc, line_ca, cross_point2);
+        Quad corner_cab = get_corner_quad(line_ca, line_ab, cross_point3);
+
+        draw_screen_rect(convert_ratio(line_ab, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(line_bc, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(line_ca, true), color, true, .0f);
+
+        draw_screen_rect(convert_ratio(corner_abc, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(corner_bca, true), color, true, .0f);
+        draw_screen_rect(convert_ratio(corner_cab, true), color, true, .0f);
     }
 }
 
 void VanillaRenderContext::draw_screen_line(const Vec2& A, const Vec2& B, Color color, float thickness)
 {
     constexpr float ratio = 16.0f / 9.0f;
-    constexpr float inverse_ratio = 9.0f / 16.0f;
 
     Vec2 new_A{A.x * ratio, A.y};
     Vec2 new_B{B.x * ratio, B.y};
-    float hypotenuse = (float)std::sqrt(std::pow(new_B.x - new_A.x, 2) + std::pow(new_B.y - new_A.y, 2));
-    float axis_AB_angle = std::atan2((new_B.y - new_A.y), (new_B.x - new_A.x));
-
-    draw_screen_rect(AABB{A.x, A.y + thickness / 2, A.x + hypotenuse * inverse_ratio, A.y - thickness / 2}, color, true, .0f, axis_AB_angle, -1.0, .0f);
+    Quad line = get_line_quad(new_A, new_B, thickness);
+    draw_screen_rect(convert_ratio(line, true), color, true, .0f);
 }
 
 void VanillaRenderContext::draw_world_texture(TEXTURE texture_id, uint8_t row, uint8_t column, float left, float top, float right, float bottom, Color color)
