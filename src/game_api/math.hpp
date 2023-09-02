b845380f77b00cf529bcf04ba627651067df2fca
Mr-Auto <pl.terminator3@gmail.com>
2023-09-02 23:44:37 +0200

    fix?

--- src/game_api/math.hpp
+++ src/game_api/math.hpp
@@ -9,7 +9,8 @@ struct Vec2
 {
     Vec2() = default;
 
-    Vec2(const Vec2&) = default;
+    Vec2(const Vec2& v)
+        : x(v.x), y(v.y){};
 
     Vec2(float x_, float y_)
         : x(x_), y(y_){};
