704d9b9ee910173a0fcfab9f194841f1bb5acf72
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2023-10-21 14:17:46 +0200

    make it big letters

--- src/game_api/script/lua_vm.cpp
+++ src/game_api/script/lua_vm.cpp
@@ -1910,13 +1910,13 @@ end
     /// Get the rva for a pattern name, used for debugging.
     lua["get_rva"] = [](std::string_view address_name) -> std::string
     {
-        return fmt::format("{:x}", get_address(address_name) - Memory::get().at_exe(0));
+        return fmt::format("{:X}", get_address(address_name) - Memory::get().at_exe(0));
     };
 
     /// Get the rva for a vtable offset and index, used for debugging.
     lua["get_virtual_rva"] = [](VTABLE_OFFSET offset, uint32_t index) -> std::string
     {
-        return fmt::format("{:x}", get_virtual_function_address(offset, index));
+        return fmt::format("{:X}", get_virtual_function_address(offset, index));
     };
 
     /// Log to spelunky.log
