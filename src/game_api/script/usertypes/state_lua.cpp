237f8c9dd1504124afaecc0dc2cd17caacff8f2c
Cosine256 <4402061+Cosine256@users.noreply.github.com>
2023-11-27 09:04:27 -0700

    Declare deprecated fade values in Lua bindings

--- src/game_api/script/usertypes/state_lua.cpp
+++ src/game_api/script/usertypes/state_lua.cpp
@@ -325,11 +325,14 @@ void register_usertypes(sol::state& lua)
     statememory_type["quest_flags"] = &StateMemory::quest_flags;
     statememory_type["presence_flags"] = &StateMemory::presence_flags;
     statememory_type["fade_value"] = &StateMemory::fade_value;
-    statememory_type["fadevalue"] = &StateMemory::fadevalue;
+    /// NoDoc
+    statememory_type["fadevalue"] = &StateMemory::fade_value;
     statememory_type["fade_timer"] = &StateMemory::fade_timer;
-    statememory_type["fadeout"] = &StateMemory::fadeout;
+    /// NoDoc
+    statememory_type["fadeout"] = &StateMemory::fade_timer;
     statememory_type["fade_length"] = &StateMemory::fade_length;
-    statememory_type["fadein"] = &StateMemory::fadein;
+    /// NoDoc
+    statememory_type["fadein"] = &StateMemory::fade_length;
     statememory_type["loading_black_screen_timer"] = &StateMemory::loading_black_screen_timer;
     statememory_type["saved_dogs"] = &StateMemory::saved_dogs;
     statememory_type["saved_cats"] = &StateMemory::saved_cats;
