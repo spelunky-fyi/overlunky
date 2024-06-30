c89fe22cc8d7ebee2c9a9d4c2364486b3fdc4898
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2024-06-30 18:45:10 +0200

    color from string

--- src/game_api/script/usertypes/entity_lua.cpp
+++ src/game_api/script/usertypes/entity_lua.cpp
@@ -28,33 +28,6 @@ namespace NEntity
 {
 void register_usertypes(sol::state& lua)
 {
-    auto color_type = lua.new_usertype<Color>("Color", sol::constructors<Color(), Color(const Color&), Color(float, float, float, float)>{}, sol::meta_function::equal_to, &Color::operator==);
-    color_type["r"] = &Color::r;
-    color_type["g"] = &Color::g;
-    color_type["b"] = &Color::b;
-    color_type["a"] = &Color::a;
-    color_type["white"] = &Color::white;
-    color_type["silver"] = &Color::silver;
-    color_type["gray"] = &Color::gray;
-    color_type["black"] = &Color::black;
-    color_type["red"] = &Color::red;
-    color_type["maroon"] = &Color::maroon;
-    color_type["yellow"] = &Color::yellow;
-    color_type["olive"] = &Color::olive;
-    color_type["lime"] = &Color::lime;
-    color_type["green"] = &Color::green;
-    color_type["aqua"] = &Color::aqua;
-    color_type["teal"] = &Color::teal;
-    color_type["blue"] = &Color::blue;
-    color_type["navy"] = &Color::navy;
-    color_type["fuchsia"] = &Color::fuchsia;
-    color_type["purple"] = &Color::purple;
-    color_type["get_rgba"] = &Color::get_rgba;
-    color_type["set_rgba"] = &Color::set_rgba;
-    color_type["get_ucolor"] = &Color::get_ucolor;
-    color_type["set_ucolor"] = &Color::set_ucolor;
-    color_type["set"] = &Color::set;
-
     /// Used in EntityDB
     lua.new_usertype<Animation>(
         "Animation",
