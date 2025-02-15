81252d633e0f2e34580a9bb38c5e60c28e40a560
Mr-Auto <36127424+Mr-Auto@users.noreply.github.com>
2025-02-15 23:02:29 +0100

    even more improvements, use enums instead of raw values

--- examples/skipintro.lua
+++ examples/skipintro.lua
@@ -1,39 +1,40 @@
 meta.name = 'Skip intro'
 meta.description = 'Jump straight to 1-1 from intro or title screen.'
 meta.author = 'Dregu'
-meta.version = '1.1'
+meta.version = '1.2'
 
 --[[ Overlunky Pro Tip: Put
 autorun_scripts = ["skipintro.lua"]
 in your overlunky.ini for this to make any sense...
 
-Quick start with Q is baked into Overlunky now, but maybe you can use this with PL. ]]
+Quick start with "Q" is baked into Overlunky now, but maybe you can use this with Playlunky. ]]
 
 local function skip()
   -- Jump to 1-1 from any screen before menu
-  if state.screen >= 0 and state.screen < 4 then
+  if state.screen >= SCREEN.LOGO and state.screen < SCREEN.MENU then
 
     -- init single player:
     state.items.player_select[1].activated = true
-    -- get character from last played game
+    state.items.player_count = 1
+    -- get character from last played game:
     state.items.player_select[1].character = ENT_TYPE.CHAR_ANA_SPELUNKY + savegame.players[1]
     state.items.player_select[1].texture = TEXTURE.DATA_TEXTURES_CHAR_YELLOW_0 + savegame.players[1]
-    state.items.player_count = 1
 
     -- init state:
-    state.screen_next = 12
+    state.screen_next = SCREEN.LEVEL
     state.world_start = 1
     state.level_start = 1
-    state.theme_start = 1
+    state.theme_start = THEME.DWELLING
     state.world_next = 1
     state.level_next = 1
-    state.theme_next = 1
-    state.quest_flags = 1
+    state.theme_next = THEME.DWELLING
+    state.quest_flags = QUEST_FLAG.RESET
     state.loading = 1
 
-    -- fix for character select screen:
+    -- fix for character select screen (when going directly from death screen):
     state.screen_character_select.available_mine_entrances = 4;
-    -- you can also set it to SCREEN.LEVEL to skip the camp
+    -- can also be set to SCREEN.LEVEL
+    -- then going to character select (from death screen) and choosing the character will bring you back to the game directly
     state.screen_character_select.next_screen_to_load = SCREEN.CAMP;
 
     -- disable the [title screen -> menu] animation:
@@ -50,10 +51,7 @@ local function skip()
   end
 end
 
-set_callback(function()
-  skip()
-  -- run only once, otherwise we can't go back to main menu
-  clear_callback()
-end, ON.SCREEN)
+set_callback(skip, ON.SCREEN)
 
+-- only for overlunky loading the script from the UI
 skip()
